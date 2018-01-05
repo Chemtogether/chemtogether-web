a2629c149890cd4b44e68cabe25be125befbe5e5
agimpel <agimpel@student.ethz.ch>
2018-01-05 23:39:52 +0100

    first implementation of sliding sidenav bar

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<!-- HEAD -->
+<head>
+  <!-- meta-tags -->
+  <meta name="keywords" content="Chemtogether, job fair, chemistry, chemical engineering, interdisciplinary, science, ETH, ETHZ"/>
+  <meta name="description" content="Chemtogether"/>
+  <meta name="author" content="webmaster@chemtogether.ethz.ch"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta charset="UTF-8"/>
+  <meta http-equiv="Cache-Control" content="max-age=172800" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <!-- title + favicon -->
+  <title>Chemtogether - 2018</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/img/favicon/manifest.json">
+  <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#272727">
+  <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+  <meta name="apple-mobile-web-app-title" content="Chemtogether">
+  <meta name="application-name" content="Chemtogether">
+  <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#272727">
+
+  <!-- css includes -->
+  <link rel="stylesheet" href="/css/main.css"/>
+
+  <!-- js includes -->
+  <script src="/js/main.js"></script>
+
+
+</head>
+
+<body>
+  <div id="sideNavigation" class="sidenav">
+    <a href="#">About</a>
+    <a href="#">Features</a>
+    <a href="#">Contact Us</a>
+  </div>
+
+  <div id="topnav">
+    <a href="#" onclick="openNav()">
+      Open
+    </a>
+  </div>
+
+  <div id="main">
+    Test  sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds
+  </div>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sideNavigation").classList.add("sidenav_open");
+  document.getElementById("main").classList.add("sidenav_open_main");
+}
+
+function closeNav() {
+  document.getElementById("sideNavigation").classList.remove("sidenav_open");
+  document.getElementById("main").classList.remove("sidenav_open_main");
+}
