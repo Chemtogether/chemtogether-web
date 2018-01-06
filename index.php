2c12708b0cbab3200fb5471c4adf4333a67c2686
agimpel <agimpel@student.ethz.ch>
2018-01-06 22:54:32 +0100

    introduced separate navigation switches for touch devices

--- index.php
+++ index.php
@@ -9,46 +9,40 @@
   <meta name="author" content="webmaster@chemtogether.ethz.ch"/>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta charset="UTF-8"/>
-  <meta http-equiv="Cache-Control" content="max-age=172800" />
+  <meta http-equiv="Cache-Control" content="max-age=0" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-  <!-- title + favicon -->
-  <title>Chemtogether - 2018</title>
-  <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="/img/favicon/manifest.json">
-  <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#272727">
-  <link rel="shortcut icon" href="/img/favicon/favicon.ico">
-  <meta name="apple-mobile-web-app-title" content="Chemtogether">
-  <meta name="application-name" content="Chemtogether">
-  <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
-  <meta name="theme-color" content="#272727">
 
   <!-- css includes -->
   <link rel="stylesheet" href="/css/main.css"/>
-
-  <!-- js includes -->
-  <script src="/js/main.js"></script>
 
 
 </head>
 
 <body>
-  <div id="sideNavigation" class="sidenav">
+  <div id="sidenav">
     <a href="#">About</a>
     <a href="#">Features</a>
     <a href="#">Contact Us</a>
   </div>
 
   <div id="topnav">
-    <a href="#" onclick="openNav()">
-      Open
+    <a href="#" onclick="switchNav()">
+      Open soifhaspoghaepo
     </a>
   </div>
 
   <div id="main">
-    Test  sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds
+    <div id="content">
+      Test  sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds
+    </div>
   </div>
+
+
+
+  <!-- js includes -->
+  <script type="text/javascript" src="/js/jquery.js"></script>
+  <script type="text/javascript" src="/js/main.js"></script>
+
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
-function openNav() {
-  document.getElementById("sideNavigation").classList.add("sidenav_open");
-  document.getElementById("main").classList.add("sidenav_open_main");
+
+
+// check if side navigation is already opened (based on whether the respective class is set) and switch state
+function switchNav() {
+  if ($("#sidenav").hasClass("sidenav_open")) {
+    closeNav();
+  } else {
+    openNav();
+  }
 }
 
+
+
+
+
+
 function closeNav() {
-  document.getElementById("sideNavigation").classList.remove("sidenav_open");
-  document.getElementById("main").classList.remove("sidenav_open_main");
+  $("#sidenav").removeClass("sidenav_open");
+  $("#main").removeClass("sidenav_open_main");
+  $("#topnav").removeClass("sidenav_open_main");
+
+  if (!!('ontouchstart' in window)) {//check for touch device
+    $("#main").unbind("click",closeNav);
+  }
+}
+
+function openNav() {
+  $("#sidenav").addClass("sidenav_open");
+  $("#main").addClass("sidenav_open_main");
+  $("#topnav").addClass("sidenav_open_main");
+
+  if (!!('ontouchstart' in window)) {//check for touch device
+    $("#main").click(closeNav);
+  }
+}
+
+
+
+
+
+if (!!('ontouchstart' in window)) {//check for touch device
+  //behaviour and events for touch device
+}
+else {
+  $("#sidenav").hover(switchNav);
 }
