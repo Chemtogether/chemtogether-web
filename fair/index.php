85e7b533211c282005486d07b901441daa18be53
agimpel <agimpel@student.ethz.ch>
2018-02-08 22:38:21 +0100

    finished template for /fair

--- fair/index.php
+++ fair/index.php
@@ -76,129 +76,197 @@ $active_nav = 'fair';
     </div>
 
     <div class="content flex">
-      <div class="text l-12m-12 s-12">
+      <div class="text l-12 m-12 s-12">
         <?php echo($lang['content']['main_text'][$eng]); ?>
       </div>
     </div>
 
-    <div class="spacer">
-    </div>
-
-    <div class="section">
-      <span class="section-slashes">
-        <span>/</span><span>/</span>
-      </span>
-      <span class="section-text">
-        Schedule
-      </span>
-    </div>
-
     <div class="content flex">
-      <div class="text l-12m-12 s-12">
-        <?php echo($lang['content']['schedule_text'][$eng]); ?>
+      <div class="text l-4 m-12 s-12 fair-introlinks">
+        <a href="https://www.ethz.ch/de/campus/standorte-anreise/standorte-ZH/hoenggerberg.html"><i class="far fa-fw fa-map"></i> <span>Get directions</span></a>
       </div>
-
-      <div class="text l-12m-12 s-12">
-        <div class="subsection">
-            Monday, 5th November
-        </div>
-        <table class="schedule">
-          <tr>
-            <td><i class="far fa-fw fa-comment"></i></td>
-            <td>18:00</td>
-            <td>ETH Career Center Talk: How to write a CV</td>
-          </tr>
-        </table>
+      <div class="text l-4 m-12 s-12 fair-introlinks">
+        <a href="/files/expoguide.pdf"><i class="far fa-fw fa-file-alt"></i> <span>Download the expo guide</span></a>
       </div>
-
-      <div class="text l-12m-12 s-12">
-        <div class="subsection">
-            Tuesday, 6th November
-        </div>
-        <table class="schedule">
-          <tr>
-            <td><i class="far fa-fw fa-clock"></i></td>
-            <td>09:00</td>
-            <td>Start of the fair</td>
-          </tr>
-          <tr>
-            <td><i class="far fa-fw fa-comment"></i></td>
-            <td>09:00</td>
-            <td>Opening Talk</td>
-          </tr>
-          <tr>
-            <td><i class="fas fa-fw fa-utensils"></i></td>
-            <td>16:00</td>
-            <td>Apéro</td>
-          </tr>
-          <tr>
-            <td><i class="far fa-fw fa-clock"></i></td>
-            <td>17:00</td>
-            <td>End of the fair</td>
-          </tr>
-        </table>
-      </div>
-
-      <div class="text l-12m-12 s-12">
-        <div class="subsection">
-            Wednesday, 7th November
-        </div>
-        <table class="schedule">
-          <tr>
-            <td><i class="far fa-fw fa-clock"></i></td>
-            <td>09:00</td>
-            <td>Start of the fair</td>
-          </tr>
-          <tr>
-            <td><i class="far fa-fw fa-comment"></i></td>
-            <td>09:00</td>
-            <td>Opening Talk</td>
-          </tr>
-          <tr>
-            <td><i class="fas fa-fw fa-utensils"></i></td>
-            <td>16:00</td>
-            <td>Apéro</td>
-          </tr>
-          <tr>
-            <td><i class="far fa-fw fa-clock"></i></td>
-            <td>17:00</td>
-            <td>End of the fair</td>
-          </tr>
-        </table>
-      </div>
-
-    </div>
-
-    <div class="spacer">
-    </div>
-
-    <div class="section">
-      <span class="section-slashes">
-        <span>/</span><span>/</span>
-      </span>
-      <span class="section-text">
-        Supporting Programme
-      </span>
-    </div>
-
-    <div class="content flex">
-      <div class="text l-6 m-12 s-12">
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-      </div>
-      <div class="text l-6 m-12 s-12">
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      <div class="text l-4 m-12 s-12 fair-introlinks">
+        <a href="/companies/"><i class="far fa-fw fa-building"></i> <span>Check out the exhibitors</span></a>
       </div>
     </div>
+  </div>
 
-    <div class="spacer">
+  <div class="spacer">
+  </div>
+
+  <div class="spacer">
+  </div>
+
+  <div class="section">
+    <span class="section-slashes">
+      <span>/</span><span>/</span>
+    </span>
+    <span class="section-text">
+      Schedule
+    </span>
+  </div>
+
+  <div class="content flex">
+    <div class="text l-12 m-12 s-12">
+      <?php echo($lang['content']['schedule_text'][$eng]); ?>
     </div>
 
-    <div class="spacer">
+    <div class="text l-12 m-12 s-12">
+      <div class="subsection">
+        Monday, 5th November
+      </div>
+      <table class="schedule">
+        <tr>
+          <td><i class="far fa-fw fa-comment"></i></td>
+          <td>18:00</td>
+          <td>ETH Career Center Talk: How to write a CV</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="text l-12 m-12 s-12">
+      <div class="subsection">
+        Tuesday, 6th November
+      </div>
+      <table class="schedule">
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td>09:00</td>
+          <td>Start of the fair</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-comment"></i></td>
+          <td>09:00</td>
+          <td>Opening Talk</td>
+        </tr>
+        <tr>
+          <td><i class="fas fa-fw fa-utensils"></i></td>
+          <td>16:00</td>
+          <td>Apéro</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td>17:00</td>
+          <td>End of the fair</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="text l-12 m-12 s-12">
+      <div class="subsection">
+        Wednesday, 7th November
+      </div>
+      <table class="schedule">
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td>09:00</td>
+          <td>Start of the fair</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-comment"></i></td>
+          <td>09:00</td>
+          <td>Opening Talk</td>
+        </tr>
+        <tr>
+          <td><i class="fas fa-fw fa-utensils"></i></td>
+          <td>16:00</td>
+          <td>Apéro</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td>17:00</td>
+          <td>End of the fair</td>
+        </tr>
+      </table>
     </div>
 
   </div>
+
+  <div class="spacer">
+  </div>
+
+  <div class="spacer">
+  </div>
+
+  <div class="section">
+    <span class="section-slashes">
+      <span>/</span><span>/</span>
+    </span>
+    <span class="section-text">
+      Supporting Programme
+    </span>
+  </div>
+
+  <div class="content flex">
+    <div class="text l-12 m-12 s-12">
+      <div class="subsection">
+        Flash presentations
+      </div>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      <br><br>
+      <table>
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td> see schedule</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-map"></i></td>
+          <td>HCI G2</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <div class="content flex">
+    <div class="text l-12 m-12 s-12">
+      <div class="subsection">
+        ETH Career Center: CV-Training
+      </div>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      <br><br>
+      <table>
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td> Monday, 6th November, at 18:00</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-map"></i></td>
+          <td>HCI tba</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <div class="content flex">
+    <div class="text l-12 m-12 s-12">
+      <div class="subsection">
+        CV-Pics: Photo-Shoot
+      </div>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      <br><br>
+      <table>
+        <tr>
+          <td><i class="far fa-fw fa-clock"></i></td>
+          <td> throughout both days, registration necessary</td>
+        </tr>
+        <tr>
+          <td><i class="far fa-fw fa-map"></i></td>
+          <td>HCI tba</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <div class="spacer">
+  </div>
+
+  <div class="spacer">
+  </div>
+
+</div>
 
 
 </div>
