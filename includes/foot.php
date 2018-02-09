4308d772bb59c730264507736a6acac4e9912d64
agimpel <agimpel@student.ethz.ch>
2018-02-09 09:45:05 +0100

    Externalized static images to AWS S3 and use CDNs for jquery/Fontawesome

--- includes/foot.php
+++ includes/foot.php
@@ -1,5 +1,5 @@
 <!-- js includes -->
-<script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="/js/main.js"></script>
 
 
--- includes/head.php
+++ includes/head.php
@@ -16,17 +16,17 @@
 
 
   <!-- title + favicon -->
-	<title><?php echo($lang['general']['page_title'][$eng]); ?></title>
-	<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
-	<link rel="manifest" href="/img/favicon/manifest.json">
-	<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#272727">
-	<link rel="shortcut icon" href="/img/favicon/favicon.ico">
-	<meta name="apple-mobile-web-app-title" content="Chemtogether">
-	<meta name="application-name" content="Chemtogether">
-	<meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
-	<meta name="theme-color" content="#272727">
+  <title><?php echo($lang['general']['page_title'][$eng]); ?></title>
+  <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/img/favicon/manifest.json">
+  <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#272727">
+  <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+  <meta name="apple-mobile-web-app-title" content="Chemtogether">
+  <meta name="application-name" content="Chemtogether">
+  <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#272727">
 
 
   <!-- css includes -->
@@ -42,7 +42,7 @@
 
   gtag('config', 'UA-2650931-4');
   </script>
-  <script type="text/javascript" src="/js/fontawesome.js"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
 
 </head>
