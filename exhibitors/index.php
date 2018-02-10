b2a73d28ed18a6f002e331abb923087678f64183
agimpel <agimpel@student.ethz.ch>
2018-02-10 22:56:24 +0100

    finished rework of /exhibitors

--- exhibitors/index.php
+++ exhibitors/index.php
@@ -45,7 +45,7 @@ $active_nav = 'exhibitors';
 
     <div class="content flex">
       <div class="text l-12 m-12 s-12">
-        <?php echo($lang['content']['intro_text'][$eng]); ?>
+        <?php echo($lang['content']['intro_text1'][$eng]); ?>
       </div>
     </div>
 
@@ -65,7 +65,7 @@ $active_nav = 'exhibitors';
           <span>Daily visitors</span>
         </div>
         <div class="image-container">
-          <span>5/6</span>
+          <span>4.5/5</span>
           <div class="bar"></div>
           <span>Ø Rating</span>
         </div>
@@ -77,7 +77,13 @@ $active_nav = 'exhibitors';
 
     <div class="content flex">
       <div class="text l-12 m-12 s-12">
-        Please do not hesitate to contact us:<br><br>
+        <?php echo($lang['content']['intro_text2'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="content flex">
+      <div class="text l-12 m-12 s-12">
+        Please do not hesitate to contact us:<br style="line-height:40px;" />
         <table>
           <tr>
             <td><i class="fa-fw far fa-envelope"></i></td>
@@ -92,7 +98,7 @@ $active_nav = 'exhibitors';
     </div>
     <div class="content flex">
       <div class="text l-12 m-12 s-12">
-        Additional documents:<br><br>
+        Additional documents:<br style="line-height:40px;" />
         <table>
           <tr>
             <td><i class="fa-fw far fa-file-alt"></i></td>
@@ -119,6 +125,212 @@ $active_nav = 'exhibitors';
       <span class="section-text">
         Partner Options
       </span>
+    </div>
+
+    <div class="content flex">
+      <div class="text l-12 m-12 s-12">
+        Here is some informational text
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="text l-4 m-6 s-12">
+        <div class="packages-base">
+          <span>C</span>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          Base Package
+        </div>
+        <table class="packages">
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 1</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 2</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 3</td>
+          </tr>
+        </table>
+        <br>
+        <span>CHF 1234.- total</span>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="text l-4 m-6 s-12">
+        <div class="packages-silver">
+          <span>Ag</span>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          Silver Package
+        </div>
+        <table class="packages">
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 1</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 2</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 3</td>
+          </tr>
+        </table>
+        <br>
+        <span>CHF 1234.- total</span>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="text l-4 m-6 s-12">
+        <div class="packages-gold">
+          <span>Au</span>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          Gold Package
+        </div>
+        <table class="packages">
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 1</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 2</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 3</td>
+          </tr>
+        </table>
+        <br>
+        <span>CHF 1234.- total</span>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="text l-4 m-6 s-12">
+        <div class="packages-platinum">
+          <span>Pt</span>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          Platinum Package
+        </div>
+        <table class="packages">
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 1</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 2</td>
+          </tr>
+          <tr>
+            <td><i class="fas fa-angle-right"></i></td>
+            <td>Item 3</td>
+          </tr>
+        </table>
+        <br>
+        <span>CHF 1234.- total</span>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="section">
+      <span class="section-slashes">
+        <span>/</span><span>/</span>
+      </span>
+      <span class="section-text">
+        Company Feedback
+      </span>
+    </div>
+
+    <div class="content flex">
+      <div class="text l-12 m-12 s-12">
+        <?php echo($lang['content']['feedback_text'][$eng]); ?>
+        <br><br>
+        <table class="feedback">
+          <tr>
+            <td>4.5</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Organisation und Kommunikation vor der Messe</td>
+          </tr>
+          <tr>
+            <td>4.8</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Betreuung während der Messe</td>
+          </tr>
+          <tr>
+            <td>4.6</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Frequentierung der Stände durch Studenten</td>
+          </tr>
+          <tr>
+            <td>4.5</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Qualität der Gespräche mit Studenten</td>
+          </tr>
+          <tr>
+            <td>4.6</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Verpflegung</td>
+          </tr>
+          <tr>
+            <td>4.3</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Standposition</td>
+          </tr>
+          <tr>
+            <td>4.3</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Preis-Leistung</td>
+          </tr>
+          <tr>
+            <td>4.5</td>
+            <td>/</td>
+            <td>5</td>
+            <td>Gesamteindruck</td>
+          </tr>
+        </table>
+      </div>
     </div>
 
     <div class="spacer">
