f3da24241980d6ebdf42d0a815b1c2d9d4a6d0e2
agimpel <agimpel@student.ethz.ch>
2018-02-17 15:33:45 +0100

    added content for /aboutus

--- aboutus/index.php
+++ aboutus/index.php
@@ -27,7 +27,7 @@ $active_nav = 'aboutus';
 
 
 <!-- START CONTENT -->
-<div id="main">
+<div id="main" class="aboutus">
 
   <div class="container">
 
@@ -44,14 +44,504 @@ $active_nav = 'aboutus';
     </div>
 
     <div class="content flex">
-      <div class="text l-6 m-12 s-12">
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      <div class="text l-12 m-12 s-12">
+        <?php echo($lang['content']['intro_text'][$eng]); ?>
       </div>
-      <div class="text l-6 m-12 s-12">
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-        Test sfdndjshflgoöhasdpfogiih püsd908hgp9dshgüä sd0gds Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer" id="team">
+    </div>
+
+    <div class="section">
+      <span class="section-slashes">
+        <span>/</span><span>/</span>
+      </span>
+      <span class="section-text">
+        <?php echo($lang['content']['team_subsection'][$eng]); ?>
+      </span>
+    </div>
+
+    <div class="content flex">
+      <div class="image-big l-12 m-12 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/team_default','jpg'); ?>></img>
+        </div>
       </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_1']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_1']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_1']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_2']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_2']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_2']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_3']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_3']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_3']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_4']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_4']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_4']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_5']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_5']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_5']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_6']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_6']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_6']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_7']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_7']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_7']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_8']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_8']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_8']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_9']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_9']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_9']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_10']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_10']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_10']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_11']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_11']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_11']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex">
+      <div class="image-member l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/team/user_default','jpg'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['team_member_12']['name']); ?>
+        </div>
+        <span><?php echo($lang['content']['team_member_12']['task'][$eng]); ?></span>
+        <br><br>
+        <?php echo($lang['content']['team_member_12']['info'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer" id="alumni">
+    </div>
+
+    <div class="section">
+      <span class="section-slashes">
+        <span>/</span><span>/</span>
+      </span>
+      <span class="section-text">
+        <?php echo($lang['content']['alumni_subsection'][$eng]); ?>
+      </span>
+    </div>
+
+    <div class="content flex">
+      <div class="text l-12 m-12 s-12">
+        <?php echo($lang['content']['alumni_text'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="content flex">
+      <div class="text l-6 m-12 s-12">
+        <div class="subsection">
+          Chemtogether 2017
+        </div>
+        <?php echo($lang['content']['alumni_member_2017']); ?>
+      </div>
+
+      <div class="text l-6 m-12 s-12">
+        <div class="subsection">
+          Chemtogether 2016
+        </div>
+        <?php echo($lang['content']['alumni_member_2016']); ?>
+      </div>
+
+      <div class="text l-6 m-12 s-12">
+        <div class="subsection">
+          Chemtogether 2015
+        </div>
+        <?php echo($lang['content']['alumni_member_2015']); ?>
+      </div>
+
+      <div class="text l-6 m-12 s-12">
+        <div class="subsection">
+          Chemtogether 2014
+        </div>
+        <?php echo($lang['content']['alumni_member_2014']); ?>
+      </div>
+
+      <div class="text l-6 m-12 s-12">
+        <div class="subsection">
+          Chemtogether 2013
+        </div>
+        <?php echo($lang['content']['alumni_member_2013']); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer" id="organisation">
+    </div>
+
+    <div class="section">
+      <span class="section-slashes">
+        <span>/</span><span>/</span>
+      </span>
+      <span class="section-text">
+        <?php echo($lang['content']['organisation_subsection'][$eng]); ?>
+      </span>
+    </div>
+
+    <div class="content flex">
+      <div class="text l-12 m-12 s-12">
+        <?php echo($lang['content']['organisation_text'][$eng]); ?>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex-center">
+      <div class="image-organisation l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/organisation/vcs','png'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['organisation']['vcs'][$eng]); ?>
+        </div>
+        <a href="https://www.vcs.ethz.ch/">www.vcs.ethz.ch</a>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex-center">
+      <div class="image-organisation l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/organisation/vac','png'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['organisation']['vac'][$eng]); ?>
+        </div>
+        <a href="https://www.vac.ethz.ch/">www.vac.ethz.ch</a>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex-center">
+      <div class="image-organisation l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/organisation/vseth','png'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['organisation']['vseth'][$eng]); ?>
+        </div>
+        <a href="http://www.vseth.ethz.ch/">www.vseth.ethz.ch</a>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex-center">
+      <div class="image-organisation l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/organisation/aveth','png'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['organisation']['aveth'][$eng]); ?>
+        </div>
+        <a href="http://www.aveth.ethz.ch/">www.aveth.ethz.ch</a>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex-center">
+      <div class="image-organisation l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/organisation/dchab','png'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['organisation']['chab'][$eng]); ?>
+        </div>
+        <a href="https://www.chab.ethz.ch/">www.chab.ethz.ch</a>
+      </div>
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="spacer">
+    </div>
+
+    <div class="content flex-center">
+      <div class="image-organisation l-4 m-6 s-12">
+        <div>
+          <img <?php set_source('https://s3.eu-central-1.amazonaws.com/static.chemtogether.2018/img/organisation/eth','png'); ?>></img>
+        </div>
+      </div>
+      <div class="text l-8 m-6 s-12">
+        <div class="subsection">
+          <?php echo($lang['content']['organisation']['eth'][$eng]); ?>
+        </div>
+        <a href="https://www.ethz.ch/">www.ethz.ch</a>
+      </div>
+    </div>
+
+    <div class="spacer">
     </div>
 
     <div class="spacer">
--- aboutus/lang.php
+++ aboutus/lang.php
@@ -9,5 +9,110 @@ $lang['navigation']['topnav_title']= array('Das Team', 'The Team');
 
 // CONTENT TRANSLATIONS FOR THIS PAGE
 
+$lang['content']['intro_text'] = array(
+  'Das Team der Chemtogether besteht aus Studenten und Doktoranden des Departments für Chemie und Angewandte Biowissenschaften der ETH Zürich, die die Organisation der Karrieremesse ehrenamtlich übernehmen. Unterstützt wird die Chemtogether von der Vereinigung der Chemiestudierenden, VCS, und der Vereinigung der Doktorierenden, VAC, sowie den entsprechenden Dachvereinen VSETH und AVETH.',
+  'The team of Chemtogether consists of students and PhDs of the department for Chemistry and Applied Biosciences of ETH Zurich, wo organize the career fair voluntarily. Chemtogether is supported by the association of chemistry students, VCS, and and the association of PhD students, VAC, as well the corresponding umbrella organisations VSETH and AVETH.'
+);
+
+$lang['content']['team_subsection'] = array('Das Team 2018','The Team of 2018');
+
+$lang['content']['team_member_1'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_2'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_3'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_4'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_5'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_6'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_7'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_8'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_9'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_10'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_11'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+$lang['content']['team_member_12'] = array(
+  'name' => 'Max Mustermann',
+  'task' => array('Test Manager','Chief Test Officer'),
+  'info' => array('Hier könnte deine Info stehen','Some info on me')
+);
+
+
+
+$lang['content']['alumni_subsection'] = array('Chemtogether Alumni','Chemtogether Alumni');
+$lang['content']['alumni_text'] = array('Ein grosses Dankeschön an alle, die an der Organisation der Chemtogether in früheren Jahren beteiligt waren!','A big thanks to everyone involved in the organisation of Chemtogether in the previous years!');
+
+$lang['content']['alumni_member_2017'] = 'Philipp Antkowiak<br>Philippe Bechtold<br>Andreas Gimpel<br>David Hahn<br>Anton Hartner<br>Lukas Lätsch<br>Josephine Scheiter<br>Erik Schrader<br>Cyril Schroeder<br>Esther Tschanen-Hammann<br>Patrik Willi<br>Konstantin Zouboulis';
+
+$lang['content']['alumni_member_2016'] = 'Philipp Antkowiak<br>Philippe Bechtold<br>Moritz Benisch<br>Frederik Eiler<br>David Hahn<br>Karina Hugentobler<br>Lukas Lätsch<br>Josephine Scheiter<br>Cyril Schroeder';
+
+$lang['content']['alumni_member_2015'] = 'Philipp Antkowiak<br>Philippe Bechtold<br>Daniel Dunkelmann<br>Frederik Eiler<br>Marco Hartmann<br>Jérôme Käslin<br>Lukas Lätsch<br>Mario Lenz<br>Jan-Grimo Sobez';
+
+$lang['content']['alumni_member_2014'] = 'Stefan Allner<br>Moritz Benisch<br>Maximilian Doppelbauer<br>Daniel Dunkelmann<br>Frederik Eiler<br>Marco Hartmann<br>Mario Lenz<br>Jan-Grimo Sobez';
+
+$lang['content']['alumni_member_2013'] = 'Stefan Allner<br>Maximilian Doppelbauer<br>Daniel Dunkelmann<br>Marco Hartmann<br>Jan-Grimo Sobez<br>Nino Willi';
+
+
+
+$lang['content']['organisation_subsection'] = array('Die Organisation','The Organisation');
+$lang['content']['organisation_text'] = array('Chemtogether ist ein gemeinsames Projekt der VCS und der VAC. Die folgenden Organisationen und Dachverbände unterstützen die Chemtogether.','Chemtogether is a joint effort of VCS and VAC. The following organisations and umbrella organisations support Chemtogether.');
+
+$lang['content']['organisation']['vcs'] = array('Vereinigung der Chemiestudierenden','Association of the Chemistry Students');
+$lang['content']['organisation']['vac'] = array('Vereinigung der Assistenten der Chemischen Labore der ETH Zürich','Union of the Assistants at the Chemical Labs of ETH Zurich');
+$lang['content']['organisation']['vseth'] = array('Verband der Studierenden an der ETH Zürich','Association of Students at ETH Zurich');
+$lang['content']['organisation']['aveth'] = array('Akademische Vereinigung des Mittelbaus der ETH Zürich','Academic Association of Scientific Staff at ETH Zurich ');
+$lang['content']['organisation']['chab'] = array('Departement Chemie und Angewandte Biowissenschaften','Department of Chemistry and Applied Biosciences');
+$lang['content']['organisation']['eth'] = array('ETH Zürich','ETH Zurich');
 
 ?>
