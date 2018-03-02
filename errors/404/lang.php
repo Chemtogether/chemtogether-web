51f7e9072db1fc8d6ba9d38de651be3680d3442e
agimpel <agimpel@student.ethz.ch>
2018-03-02 23:17:02 +0100

    added server error support

---
+++ errors/404/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+// GENERAL TRANSLATIONS FOR THIS PAGE
+$lang['general']['page_title'] = array('Chemtogether 2018', 'Chemtogether 2018');
+$lang['navigation']['title'] = array('Fehler 404', 'Error 404');
+$lang['navigation']['topnav_title']= array('Chemtogether 2018', 'Chemtogether 2018');
+
+
+
+// CONTENT TRANSLATIONS FOR THIS PAGE
+
+$lang['content'] = array(
+  'error_1' => array('Da ist was falsch gelaufen! Der gegebene Pfad existiert nicht (mehr).','Something went wrong! The path you are looking for does not exist (anymore).'),
+  'error_2' => array('Sollte dieser Fehler unerwartet sein, melde ihn bei','Should this error be unexpected, please report it to'),
+  'home' => array('zur Startseite','return to home'),
+  'return' => array('zurück zur vorigen Seite','go back to the previous page'),
+);
+
+?>
---
+++ errors/500/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+// GENERAL TRANSLATIONS FOR THIS PAGE
+$lang['general']['page_title'] = array('Chemtogether 2018', 'Chemtogether 2018');
+$lang['navigation']['title'] = array('Fehler 500', 'Error 500');
+$lang['navigation']['topnav_title']= array('Chemtogether 2018', 'Chemtogether 2018');
+
+
+
+// CONTENT TRANSLATIONS FOR THIS PAGE
+
+$lang['content'] = array(
+  'error_1' => array('Da ist was falsch gelaufen!','Something went wrong!'),
+  'error_2' => array('Sollte dieser Fehler unerwartet sein, melde ihn bei','Should this error be unexpected, please report it to'),
+  'home' => array('zur Startseite','return to home'),
+  'return' => array('zurück zur vorigen Seite','go back to the previous page'),
+);
+
+?>
