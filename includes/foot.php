2edd6c1962905d2463a7c29e66638daddc0cdc00
agimpel <agimpel@student.ethz.ch>
2018-03-18 13:07:44 +0100

    changed css and js versions

--- includes/foot.php
+++ includes/foot.php
@@ -1,6 +1,6 @@
 <!-- js includes -->
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="/js/all.js?v=20180317_0"></script>
+<script type="text/javascript" src="/js/all.js?v=20180318_0"></script>
 
 
 </body>
--- includes/head.php
+++ includes/head.php
@@ -49,7 +49,7 @@
 
 
   <!-- css includes -->
-  <link rel="stylesheet" href="/css/all.css?v=20180317_0"/>
+  <link rel="stylesheet" href="/css/all.css?v=20180318_0"/>
 
 
   <!-- js includes -->
