b68e7ef485d5f3b5f8b963915edc88e6e7416f52
agimpel <agimpel@student.ethz.ch>
2018-10-08 17:27:54 +0200

    Quickfix MSD v2

--- news/data/20180611_2.php
+++ news/data/20180611_2.php
@@ -1,11 +1,11 @@
 <?php
 
 $date = array('11. Juni 2018','11th June 2018');
-$title = array('Merck Sharp & Dohme - Silberpartner','Merck Sharp & Dohme - Silver Partner');
+$title = array('MSD - Silberpartner','MSD - Silver Partner');
 
-$text = array('Merck Sharp & Dohme nimmt an der Chemtogether 2018 als Silberpartner teil! Merck Sharp & Dohme wird am Mittwoch 7. November 2018 an der Chemtogether sein, also komm vorbei!<br>Mehr über Merck Sharp & Dohme <a href="/companies/msd/index.php#info">hier.</a>','Merck Sharp & Dohme will participate in Chemtogether 2018 as a silver partner! Merck Sharp & Dohme will be present on Wednesday, 7th November 2018, so see what they have to offer!<br>Learn more about Merck Sharp & Dohme <a href="/companies/msd/index.php#info">here.</a>');
+$text = array('MSD nimmt an der Chemtogether 2018 als Silberpartner teil! MSD wird am Mittwoch 7. November 2018 an der Chemtogether sein, also komm vorbei!<br>Mehr über MSD <a href="/companies/msd/index.php#info">hier.</a>','MSD will participate in Chemtogether 2018 as a silver partner! MSD will be present on Wednesday, 7th November 2018, so see what they have to offer!<br>Learn more about MSD <a href="/companies/msd/index.php#info">here.</a>');
 
-$logo = set_source($awss3.'/companies/msd/logo_svg','svg', FALSE);
+$logo = set_source($awss3.'/companies/msd/logo','png', FALSE);
 $content = <<<END
 
 <div class="text l-12 m-12 s-12">
