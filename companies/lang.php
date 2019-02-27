4b343af38a2f25c555ea90f32a635a4ffc2e302b
cschellh <christianschellh@gmail.com>
2019-02-27 15:12:38 +0100

    Firmen: Teilnehmer gelöscht.

--- companies/lang.php
+++ companies/lang.php
@@ -1,7 +1,7 @@
 <?php
 
 // GENERAL TRANSLATIONS FOR THIS PAGE
-$lang['general']['page_title'] = array('Chemtogether 2018 - Firmen', 'Chemtogether 2018 - Companies');
+$lang['general']['page_title'] = array('Chemtogether 2019 - Firmen', 'Chemtogether 2019 - Companies');
 $lang['navigation']['title'] = array('Die Firmen', 'The Companies');
 $lang['navigation']['topnav_title']= array('Die Firmen', 'The Companies');
 
@@ -10,15 +10,15 @@ $lang['navigation']['topnav_title']= array('Die Firmen', 'The Companies');
 // CONTENT TRANSLATIONS FOR THIS PAGE
 $lang['content'] = array(
   'intro_text' => array(
-    'Insgesamt 18 Unternehmen der chemischen und pharmazeutischen Industrie nehmen an den zwei Tagen der Chemtogether 2018 teil. Erfahre an welchem Tag die einzelnen Firmen teilnehmen, sowie was sie anbieten und wen sie suchen.',
-    'A total of 18 companies from the chemical and pharmaceutical industry will be present during the two days of Chemtogether 2018. Get to know what the individual companies offer, who they are looking for and when they attend the fair.'),
+    'Insgesamt 18 Unternehmen der chemischen und pharmazeutischen Industrie nehmen an den zwei Tagen der Chemtogether 2019 teil. Erfahre an welchem Tag die einzelnen Firmen teilnehmen, sowie was sie anbieten und wen sie suchen. Die Firmen werden veröffentlicht, sobald ihre Teilnahme feststeht.',
+    'A total of 18 companies from the chemical and pharmaceutical industry will be present during the two days of Chemtogether 2019. Get to know what the individual companies offer, who they are looking for and when they attend the fair. The companies will be published as soon as their participation is certain.'),
 
   'coming_soon' => array(
     'Noch hat kein Unternehmen seine Teilnahme endgültig bestätigt! Wir aktualisieren die Teilnehmerliste regelmässig, also folge uns auch auf <a href="https://www.facebook.com/chemtogether/">Facebook</a> und <a href="https://www.linkedin.com/company/chemtogether/">LinkedIn</a>!',
     'No company has completely confirmed its attendence yet! We will update you as soon as possible, so check our <a href="https://www.facebook.com/chemtogether/">Facebook</a> and <a href="https://www.linkedin.com/company/chemtogether/">LinkedIn</a>!'),
 
-  'day1' => array('Dienstag, 6. November','Tuesday, 6th November'),
-  'day2' => array('Mittwoch, 7. November', 'Wednesday, 7th November'),
+  'day1' => array('Dienstag, 5. November','Tuesday, 5th November'),
+  'day2' => array('Mittwoch, 6. November', 'Wednesday, 6th November'),
   'no-companies-notice' => array('Kein Unternehmen hat seine Teilnahme vollständig bestätigt.','No company has fully confirmed its attendence yet.'),
 );
 
@@ -37,11 +37,11 @@ $lang['detail']['packages']['platinum'] = array('Platinpartner','Platinum Partne
 
 $lang['detail']['archive'] = array('Archiv','Archive');
 
-$lang['detail']['day'][1] = array('Dienstag, 6. November','Tuesday, 6th November');
-$lang['detail']['day'][2] = array('Mittwoch, 7. November','Wednesday, 7th November');
+$lang['detail']['day'][1] = array('Dienstag, 5. November','Tuesday, 5th November');
+$lang['detail']['day'][2] = array('Mittwoch, 6. November','Wednesday, 6th November');
 
-$lang['detail']['day_short'][1] = array('6. November','6th November');
-$lang['detail']['day_short'][2] = array('7. November','7th November');
+$lang['detail']['day_short'][1] = array('5. November','5th November');
+$lang['detail']['day_short'][2] = array('6. November','6th November');
 
 
 $lang['detail']['we_are'] = array('We are','We are');
