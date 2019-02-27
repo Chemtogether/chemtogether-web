d2b6a48328ef01b7300d0e55b0461bf48108cf16
cschellh <christianschellh@gmail.com>
2019-02-27 09:30:09 +0100

    Home: Daten angepasst. APV in Beschreibung aufgenommen.

--- lang.php
+++ lang.php
@@ -1,8 +1,8 @@
 <?php
 
 // GENERAL TRANSLATIONS FOR THIS PAGE
-$lang['general']['page_title'] = array('Chemtogether 2018', 'Chemtogether 2018');
-$lang['navigation']['title'] = array('Chemtogether 2018', 'Chemtogether 2018');
+$lang['general']['page_title'] = array('Chemtogether 2019', 'Chemtogether 2019');
+$lang['navigation']['title'] = array('Chemtogether 2019', 'Chemtogether 2019');
 $lang['navigation']['topnav_title']= array('Home', 'Home');
 
 
@@ -15,8 +15,8 @@ $lang['content']['slideshow_linkvisitors'] = array('Informationen für Besucher'
 
 
 $lang['content']['intro_text'] = array(
-  'Zum sechsten Mal findet die Chemtogether in diesem Jahr am 6. und 7. November 2018 im G-Stock des HCI an der ETH Hönggerberg statt! An zwei Tagen präsentieren sich insgesamt achtzehn Unternehmen der chemischen und pharmazeutischen Industrie und bieten Informationen für die Karriereoptionen in ihren Firmen. Als ein gemeinsames Projekt der Studentenorganisation, VCS, und der Vereinigung der Doktorierenden, VAC, laden wir alle Studenten, Doktoranden und externe Besucher dazu ein, erste Kontakte mit möglichen Arbeitgebern auf der diesjährigen Chemtogether zu knüpfen!',
-  'In its sixth iteration, this year\'s Chemtogether will take place on the 6th and 7th November 2018 on the G-floor of the HCI building on the ETH Hönggerberg campus! Over two days, a total of eighteen companies from the chemical and pharmaceutical industry will be present and offer information on careers in their field. As a joint effort between the student association, VCS, and the association of PhD students, VAC, of the department of chemistry D-CHAB at the ETH Zürich, we welcome all students, PhDs and external visitors to forge first links with potential employers at this year\'s Chemtogether!');
+  'Zum sechsten Mal findet die Chemtogether in diesem Jahr am 5. und 6. November 2019 im G-Stock des HCI an der ETH Hönggerberg statt! An zwei Tagen präsentieren sich insgesamt achtzehn Unternehmen der chemischen und pharmazeutischen Industrie und bieten Informationen für die Karriereoptionen in ihren Firmen. Als ein gemeinsames Projekt der Studentenorganisationen, VCS und APV, und der Vereinigung der Doktorierenden, VAC, des Departments Chemie und Angewandte Biowissenschaften D-CHAB der ETH Zürich, laden wir alle Studenten, Doktoranden und externe Besucher dazu ein, erste Kontakte mit möglichen Arbeitgebern auf der diesjährigen Chemtogether zu knüpfen!',
+  'In its sixth iteration, this year\'s Chemtogether will take place on the 5th and 6th November 2019 on the G-floor of the HCI building on the ETH Hönggerberg campus! Over two days, a total of eighteen companies from the chemical and pharmaceutical industry will be present and offer information on careers in their field. As a joint effort between the student associations, VCS and APV, and the association of PhD students, VAC, of the Department of Chemistry and Applied Biosciences D-CHAB at the ETH Zürich, we welcome all students, PhDs and external visitors to forge first links with potential employers at this year\'s Chemtogether!');
 
 $lang['content']['infobox1_text'] = array(
   'Datum, Ort und Begleitprogramm',
