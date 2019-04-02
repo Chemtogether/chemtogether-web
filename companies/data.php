22cdb63b92bb499ff3c3c1dedc146b168dd6b530
cschellh <christianschellh@gmail.com>
2019-04-02 10:48:07 +0200

    About us: teamphoto added. Companies: Updated.

--- companies/data.php
+++ companies/data.php
@@ -10,6 +10,22 @@ $data['day1'][$i++] = array(
     "package" => 'silver',
   );
 
+  $data['day1'][$i++] = array(
+    "link_url" => '/companies/agilent/index.php#info',
+    "logo_url" => set_source($awss3.'/companies/agilent/logo','png', FALSE),
+    "name" => "Agilent Technologies",
+    "name_short" => "agilent",
+    "package" => 'base',
+  );
+
+  $data['day1'][$i++] = array(
+    "link_url" => '/companies/mettlertoledo/index.php#info',
+    "logo_url" => set_source($awss3.'/companies/mettlertoledo/logo','png', FALSE),
+    "name" => "Mettler Toledo",
+    "name_short" => "mettlertoledo",
+    "package" => 'base',
+  );
+
 // $lang['day1'] = null;
 
 $i = 0;
