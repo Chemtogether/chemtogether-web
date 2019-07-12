fba96927bbff779ea87a1bb8ca85fb2d1d5e2f5e
cschellh <christianschellh@gmail.com>
2019-07-12 11:32:29 +0200

    Impressions: Images added.

--- impressions/index.php
+++ impressions/index.php
@@ -51,6 +51,48 @@ $fb = array(
         <?php echo($lang['content']['intro_text'][$eng]); ?>
       </div>
     </div>
+
+
+    <div class="content flex">
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/01.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/01.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/02.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/02.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/03.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/03.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/04.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/04.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/05.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/05.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/06.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/06.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/07.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/07.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/08.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/08.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/09.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/09.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/10.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/10.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/11.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/11.jpg") ?> style="width:100%"></a>
+      </div>
+      <div class="text l-4 m-6 s-12">
+        <a href=<?php echo("$awss3/img/impressions/reduced_size/12.jpg") ?> data-lightbox="impressions" ><img src=<?php echo("$awss3/img/impressions/reduced_size/12.jpg") ?> style="width:100%"></a>
+      </div>
+    </div>
+
+
 </div>
 
 <!-- END CONTENT  -->
