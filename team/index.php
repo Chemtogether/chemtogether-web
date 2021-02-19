ce6111ad0e79ea21b7c2e7e3c1080e34f75e8aca
wejulius <juliuswenckstern@t-online.de>
2021-02-19 20:49:10 +0100

    Team: 2. Update Alumni 2020

--- team/index.php
+++ team/index.php
@@ -193,6 +193,13 @@ $active_nav = 'team';
     <div class="content flex">
     <div class="text l-6 m-12 s-12">
         <div class="subsection">
+          Chemtogether 2020
+        </div>
+        <?php echo($lang['content']['alumni_member_2020']); ?>
+      </div>
+
+    <div class="text l-6 m-12 s-12">
+        <div class="subsection">
           Chemtogether 2019
         </div>
         <?php echo($lang['content']['alumni_member_2019']); ?>
