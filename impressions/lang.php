5185548cd33c3513fcc48efc1e94157910f550a5
wejulius <juliuswenckstern@t-online.de>
2021-02-20 19:13:27 +0100

    Impressions: Update 2021

--- impressions/lang.php
+++ impressions/lang.php
@@ -7,8 +7,8 @@ $lang['navigation']['topnav_title']= array('Impressionen', 'Impressions');
 
 // CONTENT TRANSLATIONS FOR THIS PAGE
 $lang['content']['intro_text'] = array(
-    'Im letzten Jahr zählte die Chemtogether rund 500 Besucher an den zwei Messetagen im HCI Gebäude der ETH Hönggerberg. Hier findest Du Impressionen vom Messebereich und dem Begleitprogramm.',
-    'Last year Chemtogether counted 500 visitors during its two fair days in the HCI building at ETH Hönggerberg. Here you can find impressions of the exhibition area and the accompanying events.');
+    'Im vorletzten Jahr zählte die Chemtogether rund 500 Besucher an den zwei Messetagen im HCI Gebäude der ETH Hönggerberg. Hier findest Du Impressionen vom Messebereich und dem Begleitprogramm.',
+    'The Chemtogether 2019 counted 500 visitors during its two fair days in the HCI building at ETH Hönggerberg. Here you can find impressions of the exhibition area and the accompanying events.');
 
 $lang['content']['gallery_subsection'] = array('Galerie','Gallery');
   
