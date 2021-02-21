2b7725bb09adf046be63c118645046b6d772c92c
wejulius <juliuswenckstern@t-online.de>
2021-02-21 10:36:23 +0100

    Update: Visitors

--- visitors/lang.php
+++ visitors/lang.php
@@ -1,7 +1,7 @@
 <?php
 
 // GENERAL TRANSLATIONS FOR THIS PAGE
-$lang['general']['page_title'] = array('Chemtogether 2020 - Für Besucher', 'Chemtogether 2020 - For Visitors');
+$lang['general']['page_title'] = array('Chemtogether 2021 - Für Besucher', 'Chemtogether 2021 - For Visitors');
 $lang['navigation']['title'] = array('Für Besucher', 'For Visitors');
 $lang['navigation']['topnav_title']= array('Für Besucher', 'For Visitors');
 
@@ -15,8 +15,8 @@ $lang['content']['intro_text'] = array(
   'Chemtogether is a career fair at ETH Zürich with the aim of connecting students and companies in the general field of chemistry and pharmacy. During both days, 9 companies will present themselves and allow for the exchange of career options with the students and PhDs. In total, 18 companies from the industrial world of chemistry and pharmacy will be present.');
 
 $lang['content']['main_text'] = array(
-  'Chemtogether 2020 wird am 3. und 4. November 2020 im G-Stock des HCI-Gebäudes der ETH Hönggerberg stattfinden. An beiden Tagen werden jeweils von 9-17 Uhr die weiter unten aufgeführten neun Firmen anwesend sein. Jeder Tag beginnt mit Kaffee &amp; Croissants und endet mit einem Apéro. Die Messe ist offen für jeden, es ist also keine Registrierung notwendig.',
-  'Chemtogether 2020 will take place on 3rd and 4th November 2020 on the G-floor of the HCI building at the ETH Hönggerberg campus. On each of the two days, the nine different companies shown below will be present from 9am to 5pm. Each day will begin with coffee &amp; croissants and end with an apéro. The fair is open for everyone, so no registration is necessary.');
+  'Chemtogether 2021 wird am 2. und 3. November 2021 im G-Stock des HCI-Gebäudes der ETH Hönggerberg stattfinden. An beiden Tagen werden jeweils von 9-17 Uhr die weiter unten aufgeführten neun Firmen anwesend sein. Jeder Tag beginnt mit Kaffee &amp; Croissants und endet mit einem Apéro. Die Messe ist offen für jeden, es ist also keine Registrierung notwendig.',
+  'Chemtogether 2021 will take place on 2nd and 3rd November 2021 on the G-floor of the HCI building at the ETH Hönggerberg campus. On each of the two days, the nine different companies shown below will be present from 9am to 5pm. Each day will begin with coffee &amp; croissants and end with an apéro. The fair is open for everyone, so no registration is necessary.');
 
 $lang['content']['links_directions'] = array('Infos zur Anreise','Get directions');
 $lang['content']['links_guide'] = array('Messeführer (2019)','Expo Guide (2019)');
@@ -43,8 +43,8 @@ $lang['content']['companies_text'] = array(
     'Insgesamt 18 Unternehmen der chemischen und pharmazeutischen Industrie nehmen voraussichtlich 2021 wieder an den zwei Tagen der Chemtogether teil. Erfahre an welchem Tag die einzelnen Firmen teilnehmen, sowie was sie anbieten und wen sie suchen. Die Firmen werden veröffentlicht, sobald ihre Teilnahme feststeht.',
     'A total of 18 companies from the chemical and pharmaceutical industry will prospectively be present during the two days of Chemtogether 2021. Get to know what the individual companies offer, who they are looking for and when they attend the fair. The companies will be published as soon as their participation is certain.');
 
-  $lang['content']['day1'] = array('Dienstag, 3. November','Tuesday, 3rd November');
-  $lang['content']['day2'] = array('Mittwoch, 4. November', 'Wednesday, 4th November');
+  $lang['content']['day1'] = array('Dienstag, 2. November','Tuesday, 2nd November');
+  $lang['content']['day2'] = array('Mittwoch, 3. November', 'Wednesday, 3rd November');
 
   $lang['content']['no-companies-notice'] = array(
     'Noch hat kein Unternehmen seine Teilnahme endgültig bestätigt! Wir aktualisieren die Teilnehmerliste regelmässig, also folge uns auch auf <a href="https://www.facebook.com/chemtogether/">Facebook</a> und <a href="https://www.linkedin.com/company/chemtogether/">LinkedIn</a>!',
@@ -59,11 +59,11 @@ $lang['detail']['packages']['platinum'] = array('Platinpartner','Platinum Partne
   
 $lang['detail']['archive'] = array('Archiv','Archive');
   
-$lang['detail']['day'][1] = array('Dienstag, 3. November','Tuesday, 3rd November');
-$lang['detail']['day'][2] = array('Mittwoch, 4. November','Wednesday, 4th November');
+$lang['detail']['day'][1] = array('Dienstag, 2. November','Tuesday, 2nd November');
+$lang['detail']['day'][2] = array('Mittwoch, 3. November','Wednesday, 3rd November');
   
-$lang['detail']['day_short'][1] = array('3. November','3rd November');
-$lang['detail']['day_short'][2] = array('4. November','4th November');
+$lang['detail']['day_short'][1] = array('2. November','2nd November');
+$lang['detail']['day_short'][2] = array('3. November','3rd November');
  
 $lang['detail']['we_are'] = array('We are','We are');
 $lang['detail']['we_offer'] = array('We offer','We offer');
