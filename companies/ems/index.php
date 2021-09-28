2ec737818bb29aa2267f32f24e219ed22e3cc2e8
wejulius <juliuswenckstern@t-online.de>
2021-09-28 10:16:35 +0200

    Visitors: Update EMS

--- companies/ems/index.php
+++ companies/ems/index.php
@@ -48,14 +48,14 @@ $template['linkedin'] = array('https://www.linkedin.com/company/ems-group/','EMS
 $template['archive'] = '/archive/ems';
 
 $template['we_are'] = array(
-  'title' => '',
-  'text' => '');
+  'title' => 'We are',
+  'text' => 'The EMS Group is active worldwide in the business areas High Performance Polymers and Specialty Chemicals. The companies within the group are consolidated in the EMS-CHEMIE HOLDING AG domiciled in Domat/Ems. The business area High Performance Polymers is comprised of EMS-GRIVORY and EMS-EFTEC. EMS-GRIVORY is a leading specialised manufacturer of high-performance polyamides and the supplier with the widest range of polyamide materials. EMS-EFTEC is represented under the name EFTEC as leading supplier worldwide for adhesives, sealants and coatings including application engineering systems for the automotive industry. The business area Specialty Chemicals is made up of the Business Units EMS- GRILTECH and EMS-PAT-VAG. EMS-GRILTECH is specialized in the development and production of fibres, fusible adhesives and adhesive yarn adhesion promoters for the tyre industry, powder coatings and reactive and diluents. EMS-PATVAG specialises in ignitors for airbag gas generators. EMS has 26 production sites in 16 countries.');
 $template['we_offer'] = array(
-  'title' => '',
-  'text' => '');
+  'title' => 'We offer',
+  'text' => 'Besides jobs we also offer internships in the following areas for high school graduates, students and graduates: laboratory (chemistry), business economics, application technology, engineering/system technology/IT as preparation for the ETH. We offer you fair conditions of employment, a salary based on performance and function as well as an individual support and mentorship through hands-on training. In addition, EMS is characterised by a well-defined and lively corporate culture.');
 $template['we_look'] = array(
-  'title' => '',
-  'text' => '');
+  'title' => 'We are looking for',
+  'text' => 'We are searching for committed and qualified employees to strengthen our leading technological position and to produce products fulfilling the highest quality requirements. We expect a strong innovative drive and the will to achieve the extraordinary from our employees. We promote and demand a high degree of identification with the company and its goals to ensure future success of our strategies.');
 
 
 
