7bc8348858314570dbea3c8aeb392395ad50a1aa
wejulius <juliuswenckstern@t-online.de>
2021-10-01 16:01:37 +0200

    Update companies

--- companies/mettlertoledo/index.php
+++ companies/mettlertoledo/index.php
@@ -46,28 +46,28 @@ $template['facebook'] = array('https://www.facebook.com/mettlertoledo/', 'METTLE
 $template['linkedin'] = array('https://ch.linkedin.com/company/mettlertoledo','Mettler-Toledo International, Inc');
 
 
-$template['we_are'] = array(
-  'title' => '',
-  'text' => '');
-$template['we_offer'] = array(
-  'title' => '',
-  'text' => '');
-$template['we_look'] = array(
-  'title' => '',
-  'text' => '');
-
-
 // $template['we_are'] = array(
-//   'title' => 'We are',
-//   'text' => 'We are the leading manufacturer and provider of precision instruments. Our weighing systems and analytical instruments are found in laboratories, industrial plants, and retail industry across the globe.');
-
+//   'title' => '',
+//   'text' => '');
 // $template['we_offer'] = array(
-//   'title' => 'We offer',
-//   'text' => 'We offer challenging positions, attractive working conditions and good training opportunities in an innovative and world-leading high-tech company.');
-
+//   'title' => '',
+//   'text' => '');
 // $template['we_look'] = array(
-//   'title' => 'We are looking for',
-//   'text' => 'We are looking for a motivated personality who is always ready to develop further, who is open to new ideas and who enjoys working in a dynamic environment and realising his potential.');
+//   'title' => '',
+//   'text' => '');
+
+
+$template['we_are'] = array(
+   'title' => 'We are',
+   'text' => 'We are the leading manufacturer and provider of precision instruments. Our weighing systems and analytical instruments are found in laboratories, industrial plants, and retail industry across the globe.');
+
+$template['we_offer'] = array(
+   'title' => 'We offer',
+   'text' => 'We offer challenging positions, attractive working conditions and good training opportunities in an innovative and world-leading high-tech company.');
+
+$template['we_look'] = array(
+   'title' => 'We are looking for', 
+   'text' => 'We are looking for a motivated personality who is always ready to develop further, who is open to new ideas and who enjoys working in a dynamic environment and realising his potential.');
 
 $template['interview_print_until'] = 0;
   if(isset($_GET['preview'])) $template['interview_print_until'] = 0;
