49a3d352ce0e73307ae91e6469d8cb891b672af6
wejulius <juliuswenckstern@t-online.de>
2021-10-25 21:09:34 +0200

    update CA

--- companies/carbogenamcis/index.php
+++ companies/carbogenamcis/index.php
@@ -69,10 +69,102 @@ $template['we_look'] = array(
   'title' => 'We are looking for',
   'text' => 'We are looking for highly motivated individuals prepared to expand their high-class education & knowledge in Pharma Industry. You should be willing to add upon what you have already learned helping us and our worldwide customers to bring their products to the market relating to all aspects of modern drug development in terms of all chemistry, analytics, production, quality and regulatory aspects.');
   
-  $template['interview_print_until'] = 0;
+  $template['interview_print_until'] = 1;
   //if(isset($_GET['preview']) && $_GET['preview'] == sha1($template['company_name_short'])) $template['interview_print_until'] = 1;
 
   $i=0;
+  $template['interview'][$i++] = array(
+    'name' => 'Dr. Volker Wolfart',
+    'img_url' => $awss3.'/companies/carbogenamcis/interviewee_2021_1',
+    'position' => 'Senior Scientific Advisor',
+  
+    'education' => array(
+      0 => array(
+        'date' => '1995-1997',
+        'desc' => 'PhD in Chemistry with Prof. Dr. R. Gleiter at Universität Heidelberg',
+      ),
+      1 => array(
+        'date' => '1989-1995',
+        'desc' => 'Diploma in Chemistry at Universität Heidelberg',
+      ),
+    ),
+  
+    'career' => array(
+      0 => array(
+        'date' => '2014',
+        'desc' => 'Business Development at CARBOGEN AMCIS AG',
+      ),
+      1 => array(
+        'date' => '2011-2014',
+        'desc' => 'Senior Scientific Advisor at CARBOGEN AMCIS AG',
+      ),
+      2 => array(
+        'date' => '2008-2011',
+        'desc' => 'Head of Process Research & Development at CAROGEN AMCIS AG',
+      ),
+      3 => array(
+        'date' => '2000-2011',
+        'desc' => 'Group Manager API Synthesis at CARBOGEN AMCIS AG',
+      ),
+      4 => array(
+        'date' => '1998-2000',
+        'desc' => 'Project team leader API Synthesis at CARBOGEN AMCIS AG',
+      ),
+      5 => array(
+        'date' => '1980-1983',
+        'desc' => 'Chemical laboratory assistant at BASF',
+      ),
+    ),
+  
+    'interview' => array(
+      0 => array(
+        'question' => 'Was ist ihre derzeitige Tätigkeit bei CARBOGEN AMCIS?',
+        'answer' => 'Meine derzeitige Jobbezeichnung ist «Senior Scientific Advisor», d.h. eine Instanz für wissenschaftliche Fragen. Dazu bin ich der Koordinator für zwei der vier Schweizer Niederlassungen und im Hauptgeschäft für die Wachstumsprojekte (Business Development) der Firma verantwortlich.',
+      ),
+      1 => array(
+        'question' => 'Welchen Rat würden Sie einem „frischen“ ETH Absolventen geben, der sich auf Jobsuche begibt?',
+        'answer' => 'Auf jeden Fall mit uns in Kontakt treten und sich persönlich erkundigen, wie die momentane Situation aussieht. Wir sind stetig am Wachsen und suchen frische und begeisterungsfähige Persönlichkeiten.  …und natürlich zu uns auf die Chemtogether 2021 kommen.',
+      ),
+      2 => array(
+        'question' => 'Wie sieht Ihr klassischer Arbeitstag bei CARBOGEN AMCIS aus?',
+        'answer' => 'Den klassischen Tag gibt es nicht; es ist sehr abwechslungsreich bei CARBOGEN AMCIS zu arbeiten. Einmal der Umgang mit den Kunden, Behörden, Dienstleistern und natürlich Arbeitskollegen/-innen aus den verschiedenen Abteilungen. Chemie spielt bei mir nur noch eine untergeordnete Rolle, aber sie ist dennoch bei manchen Entscheidungen sehr wichtig.',
+      ),
+      3 => array(
+        'question' => 'Wie kam es, dass sie nun für CARBOGEN AMCIS arbeiten? Wie sind sie auf die Firma aufmerksam geworden?',
+        'answer' => 'Ich war gerade im Post Doc mit der BASF in Ludwigshafen und mein Doktorvater fand eine Anzeige in der Basler Zeitung, die CARBOGEN AMCIS geschaltet hatte. Der Schwerpunkt Organische Chemie und die Fragestellungen im Bereich organische Synthese haben mich gereizt und mich zur Bewerbung motiviert.',
+      ),
+      4 => array(
+        'question' => 'Wie lief ihr Bewerbungsprozess bei CARBOGEN AMCIS?',
+        'answer' => 'Ich wurde 1998 per Email eingeladen und durfte mich dann vor Ort persönlich vorstellen. Dabei hatte ich Kontakt mit dem Abteilungsleiter für die chemische Entwicklung, diversen künftigen Kollegen und dem damaligen Human Resources. Ausserdem konnte ich meine Expertise und Doktorarbeit im Kreise der Fachabteilung per Vortrag präsentieren. Daran hat sich bis heute wenig geändert.',
+      ),
+      5 => array(
+        'question' => 'Was glauben Sie unterscheidet CARBOGEN AMCIS von anderen Chemiefirmen? Was sind die drei grössten Vorteile von CARBOGEN AMCIS?',
+        'answer' => 'Uns unterscheidet, dass wir einen sehr starken Kundenfokus haben. Das macht es, je nach Herkunft und Background des Kunden, sehr spannend. Das Ziel ist ganz klar, den Kunden optimal zu bedienen. Ausserdem sind wir eine der wenigen weltweiten Spezialisten für hochaktive Arzneistoffe. Durch unsere internationale Vernetzung mit der Corporate Struktur (i.e. unsere weltweiten Standorte), haben wir einige schnelle Alternativen im Vergleich zu anderen Firmen der Branche. Die drei grössten Vorteile:
+
+        <table class="fa-table">
+        <tr>
+        <td><i class="fas fa-fw fa-chevron-right"></td>
+        <td>Ausgezeichnetes Arbeitsklima</td>
+        </tr>
+        <tr>
+        <td><i class="fas fa-fw fa-chevron-right"></td>
+        <td>Abwechslungsreiches Arbeitsfeld und Entwicklungsmöglichkeiten</td>
+        </tr>
+        <tr>
+        <td><i class="fas fa-fw fa-chevron-right"></td>
+        <td>High Class wissenschaftliches Arbeiten und Know-How </td>
+        </tr></table>'
+      ),
+      6 => array(
+        'question' => 'Was haben sie studiert und haben Sie sich durch das Studium gut auf Ihre jetzige Tätigkeit vorbereitet gefühlt?',
+        'answer' => 'Das Studium rüstet gut für die Arbeit in der organischen Synthese aus. Ich habe in Heidelberg studiert/promoviert. Dort wird die Organische Synthese gepflegt und hat lange Tradition. Zusätzlich braucht es aber später im Beruf Verständnis für GMP, Ökonomie, Prozesssicherheit und Personalführung, die i.d.R. im Studium nicht vermittelt werden. Dazu geben wir natürlich künftigen Kollegen*innen Möglichkeiten zur Weiterbildung. Was jedoch zählt, sind die «Soft Skills», wie der Umgang mit Kollegen und Menschen generell. Da kann man schon punkten, wenn man/frau dies auch im Studium schon geleistet hat; Bsp. durch Assistenztätigkeit, Tutorbetrieb oder andere nebenamtliche Tätigkeiten.',
+      ),
+    ),
+  );
+
+
+
+  
   $template['interview'][$i++] = array(
     'name' => 'Dr. Volker Wolfart',
     'img_url' => $awss3.'/companies/carbogenamcis/interviewee_2019_1',
