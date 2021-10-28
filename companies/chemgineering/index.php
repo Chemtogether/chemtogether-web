d44f84f07f1c44c93b084158cc8827c50f40fa33
wejulius <juliuswenckstern@t-online.de>
2021-10-28 09:17:26 +0200

    update interview chemgineering

--- companies/chemgineering/index.php
+++ companies/chemgineering/index.php
@@ -81,10 +81,71 @@ $template['we_look'] = array(
   'title' => 'We are looking for',
   'text' => 'Are you on the verge of getting your science degree, and might you have already gained your first experiences in life sciences so that you now really want to get going? Career starters have found exactly the right place here! Depending on company area, you can start as a project engineer or a junior consultant within the Chemgineering Group, and in this way get right into the middle of a project.');
 
-$template['interview_print_until'] =0;
+$template['interview_print_until'] =1;
 //if(isset($_GET['preview']) && $_GET['preview'] == sha1($template['company_name_short'])) $template['interview_print_until'] = 1;
 
 $i=0;
+
+
+$template['interview'][$i++] = array(
+  'name' => 'Helena Wiemeyer',
+  'img_url' => $awss3.'/companies/chemgineering/interviewee_2021_1',
+  'position' => 'Projektingenieur',
+
+  'education' => array(
+    0 => array(
+      'date' => '2016-2018',
+      'desc' => 'Master of Science ETH in Process Engineering, ETH Zurich, Switzerland',
+    ),
+    1 => array(
+      'date' => '2013-2016',
+      'desc' => 'Bachelor of Science ETH in Chemieingenieurwissenschaften, ETH Zurich, Switzerland',
+    ),
+  ),
+
+  'career' => array(
+    0 => array(
+      'date' => 'seit 2018',
+      'desc' => 'Projektingenieur, Chemgineering, Münchenstein',
+    ),
+    1 => array(
+      'date' => '2017-2018',
+      'desc' => 'Praktikum in der Prozessentwicklung, Roche, Basel',
+    ),
+  ),
+
+  'interview' => array(
+    0 => array(
+      'question' => 'Was ist ihre derzeitige Tätigkeit bei Chemgineering?',
+      'answer' => 'Ich arbeite als Projektingenieur in einem Grossprojekt. Neben meinem Arbeitspaket, einem Apparat für die Fest-Flüssig Trennung, bin ich für die Termin- und Qualitätskontrolle der Lieferanten zuständig.',
+    ),
+    1 => array(
+      'question' => 'Welchen Rat würden Sie einem „frischen“ ETH Absolventen geben, der sich auf Jobsuche begibt?',
+      'answer' => 'Es gibt so viele Möglichkeiten in der Industrie und an der Uni – Wo liegen deine Interessen und was macht dir Spass? In dem Bereich würde ich mir etwas suchen.',
+    ),
+    2 => array(
+      'question' => 'Wie sieht Ihr klassischer Arbeitstag bei Chemgineering aus?',
+      'answer' => 'Die meisten Tage verbringe ich in unserem Büro in Münchenstein. Da ich ein Morgenmensch bin und wir im Gleitzeitmodell arbeiten, fange ich früh an. Meetings mit meinen Kollegen im Projekt gehören genauso dazu, wie solche mit externen Partnerfirmen und Kunden – seit Beginn der Pandemie vermehrt digital. Diesen Frühling habe ich den Bau und die Inbetriebnahme bei einem Kunden vor Ort betreut und sass nur selten an meinem Schreibtisch im Büro.',
+    ),
+    3 => array(
+      'question' => 'Wie kam es, dass sie nun für Chemgineering arbeiten? Wie sind sie auf die Firma aufmerksam geworden?',
+      'answer' => 'Während des Masters habe ich habe ein Praktikum bei Roche in der technischen Prozessentwicklung gemacht. Für meinen Einstieg habe ich nach einem Planungsunternehmen und einer Stelle im Projektumfeld gesucht, da der Arbeitsalltag sehr vielfältig und abwechslungsreich ist. Mein erster Kontakt mit Chemgineering war an der Polymesse als ich noch im Bachelor war. Als ich zwei Jahre später auf Stellensuche war und mich bei den verschiedenen Dienstleistern in und um Basel umgeschaut habe, habe ich mich dann bei Chemgineering beworben.',
+    ),
+    4 => array(
+      'question' => 'Wie lief ihr Bewerbungsprozess bei Chemgineering?',
+      'answer' => 'Ich habe mich auf die Stelle beworben, die auf der Website ausgeschrieben war. Nach einem kurzen Telefonat wurde ich zu einem ersten Vorstellungsgespräch eingeladen. Anschliessend hatte ich noch mehrere Gespräche mit Führungspersonen aus den verschiedenen Firmenbereichen. Ich habe mich dann für den Bereich Prozess entschieden, da dieser am nächsten an meinem Studienfach liegt.',
+    ),
+    5 => array(
+      'question' => 'Was glauben Sie unterscheidet Chemgineering von anderen Chemiefirmen? Was sind die drei grössten Vorteile von Chemgineering?',
+      'answer' => 'Wir sind ein Dienstleister und kommen mit den produzierenden Chemiefirmen über die Projektarbeit in Kontakt. Das bedeutet, dass man Einblick in verschiedene Bereiche und Firmen bekommt und durch die wechselnden Projekte immer wieder dazulernt – für mich das wichtigste an meiner Arbeit. Auch das familiäre Umfeld in der Firma und das gute Klima schätze ich.',
+    ),
+    6 => array(
+      'question' => 'Was haben sie studiert und haben Sie sich durch das Studium gut auf Ihre jetzige Tätigkeit vorbereitet gefühlt?',
+      'answer' => 'Ich habe an der ETH Chemieingenieurwissenschaften (BSc) und Verfahrenstechnik (MSc) studiert. Wie damals im Studium, begegnen mir bei der Arbeit komplexe Fragestellungen und verschiedene Produktionsprozesse aus der Chemie und Pharmaindustrie. Das technische Knowhow aus den Vorlesungen zusammen mit den Fähigkeiten aus Übungen und Thesen, kann ich auf „echte“ Projekte anwenden. Neu waren für mich die Zusammenarbeit mit dem Kunden und die Kosten und Termine im Blick zu behalten.',
+    ),
+  ),
+);
+
 
 $template['interview'][$i++] = array(
     'name' => 'Dr. Gunnar Zoch',
