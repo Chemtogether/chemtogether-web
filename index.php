b7096efadf6e6c2698c6388b4f09ada6b97abc1e
voelkerl <voelkerl@student.ethz.ch>
2022-01-11 19:30:08 +0100

    Home - Remove big day announcement

--- index.php
+++ index.php
@@ -52,11 +52,11 @@ $fb = array(
         <li><span></span></li>
       </ul>
       <div id="home-top">
-        <div class="date-container">
+         <!-- <div class="date-container">
           <span id="date-day1">2.</span>
           <span class="date-bar"></span>
           <span id="date-day2">3.</span>
-        </div>
+         </div> -->
         <div class="date-container">
           <span id="date-month">Nov.</span>
           <span id="date-year">2022</span>
