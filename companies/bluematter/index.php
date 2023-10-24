f6878f4d3dab9ff8cc8ecb9d484713dd18d7d1e9
StefanPSchmid <schmiste@ethz.ch>
2023-10-24 22:56:40 +0200

    updated lists in company descriptions

--- companies/bluematter/index.php
+++ companies/bluematter/index.php
@@ -52,28 +52,44 @@ $template['we_are'] = array(
 
 $template['we_offer'] = array(
    'title' => 'We offer',
-   'text' => '<ul>
-   <li>A steep learning curve working on interesting and challenging projects across our clients’ products, portfolios, and organizations.</li>
-   <li>Projects from biotechs startups to multinational pharmaceutical corporations. </li>
-   <li>A Supportive environment to learn and grow, regardless of your skill and knowledge level. </li>
-   <li>Being empowered to drive your development, own your work, and manage your time from day one. </li>
-   <li>We support the wellbeing of our employees by reimbursing health-related aspects such as gym membership, sports coaching, or sports equipment. </li>
-   <li>Cost-matching education initiatives (even if it is not work-related) for your self-improvement. </li>
-   <li>Hybrid office and home working model (depending on project schedules), with very limited travel requirement (5-10% on average). </li>
-   <li>A structured onboarding program to help you learn basic consulting skills. </li>
-   <li>Support for your developmental journey with a Peer-level buddy to help you navigate your way around Blue Matter and share their experiences, as well as a senior-level career coach to support your overall professional development and career progression </li>
-   </ul>');
-   'text' => '<ul>
-   <li>A steep learning curve working on interesting and challenging projects across our clients’ products, portfolios, and organizations.</li>
-   <li>Projects from biotechs startups to multinational pharmaceutical corporations. </li>
-   <li>A Supportive environment to learn and grow, regardless of your skill and knowledge level. </li>
-   <li>Being empowered to drive your development, own your work, and manage your time from day one. </li>
-   <li>We support the wellbeing of our employees by reimbursing health-related aspects such as gym membership, sports coaching, or sports equipment. </li>
-   <li>Cost-matching education initiatives (even if it is not work-related) for your self-improvement. </li>
-   <li>Hybrid office and home working model (depending on project schedules), with very limited travel requirement (5-10% on average). </li>
-   <li>A structured onboarding program to help you learn basic consulting skills. </li>
-   <li>Support for your developmental journey with a Peer-level buddy to help you navigate your way around Blue Matter and share their experiences, as well as a senior-level career coach to support your overall professional development and career progression </li>
-   </ul>');
+   'text' => '
+   <table class="fa-table">
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>A steep learning curve working on interesting and challenging projects across our clients’ products, portfolios, and organizations.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>Projects from biotechs startups to multinational pharmaceutical corporations.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>A Supportive environment to learn and grow, regardless of your skill and knowledge level.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>Being empowered to drive your development, own your work, and manage your time from day one.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>We support the wellbeing of our employees by reimbursing health-related aspects such as gym membership, sports coaching, or sports equipment.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>Cost-matching education initiatives (even if it is not work-related) for your self-improvement.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>Hybrid office and home working model (depending on project schedules), with very limited travel requirement (5-10% on average).</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>A structured onboarding program to help you learn basic consulting skills.</td>
+  </tr>
+  <tr>
+  <td><i class="fas fa-fw fa-chevron-right"></td>
+  <td>Support for your developmental journey with a Peer-level buddy to help you navigate your way around Blue Matter and share their experiences, as well as a senior-level career coach to support your overall professional development and career progression</td>
+  </tr></table>');
 
 $template['we_look'] = array(
   'title' => 'We are looking for',
