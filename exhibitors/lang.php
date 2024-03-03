c9108bfb7efde86f759785d8468ecb011cc73b8c
StefanPSchmid <schmiste@ethz.ch>
2024-03-03 16:45:24 +0100

    Remove CT 2023 content from exhibitors page

--- exhibitors/lang.php
+++ exhibitors/lang.php
@@ -1,7 +1,7 @@
 <?php
 
 // GENERAL TRANSLATIONS FOR THIS PAGE
-$lang['general']['page_title'] = array('Chemtogether 2023 - Aussteller', 'Chemtogether 2023 - Exhibitors');
+$lang['general']['page_title'] = array('Chemtogether 2024 - Aussteller', 'Chemtogether 2024 - Exhibitors');
 $lang['navigation']['title'] = array('Für Firmen', 'For Companies');
 $lang['navigation']['topnav_title']= array('Für Firmen', 'For Companies');
 
@@ -14,8 +14,8 @@ $lang['content'] = array(
     'At Chemtogether you can meet talented future employees by direct personal interaction with students and PhDs alike. The format of the fair is such that presentation chances are equal for all companies, big and small. Here, you can win over students for your business area at the very beginning of their career. Pull in young talent by fascinating them with your research and business, opening a window to future opportunities.'),
 
   'intro_text2' => array(
-    'Chemtogether ist besonders, weil die Departemente der Chemie und Pharmazie der ETH zu den besten der Welt gehören. Chemtogether wird von einem Fachverein organisiert und ist daher preiswert. Die informelle Atmosphäre der Messe bietet eine ideale Umgebung, um mit unseren Studenten Kontakt aufzunehmen. Folgen Sie der Anleitung zur Registrierung in unserer Broschüre, um als Aussteller Teil der Chemtogether zu werden:',
-    'Chemtogether is the place to go because the chemistry and pharmacy department of the ETH is among the very best of the world. Since Chemtogether is organized by a students association, it provides excellent value for money. The informal atmosphere at Chemtogether creates the ideal framework to establish personal contact with our students. Follow the registration instructions in our brochure to become a part of Chemtogether:'),
+    'Chemtogether ist besonders, weil die Departemente der Chemie und Pharmazie der ETH zu den besten der Welt gehören. Chemtogether wird von einem Fachverein organisiert und ist daher preiswert. Die informelle Atmosphäre der Messe bietet eine ideale Umgebung, um mit unseren Studenten Kontakt aufzunehmen.', // Folgen Sie der Anleitung zur Registrierung in unserer Broschüre, um als Aussteller Teil der Chemtogether zu werden:',
+    'Chemtogether is the place to go because the chemistry and pharmacy department of the ETH is among the very best of the world. Since Chemtogether is organized by a students association, it provides excellent value for money. The informal atmosphere at Chemtogether creates the ideal framework to establish personal contact with our students.'), // Follow the registration instructions in our brochure to become a part of Chemtogether:'),
 
   'links_registration' => array('Zur Broschüre','View our brochure here'),
   'button_registration' => array('Zur Broschüre','View our brochure here'),
@@ -29,12 +29,12 @@ $lang['content'] = array(
     'Additional information:'),
 
   'documents_broschure' => array(
-    'Broschüre für Firmen (2023)',
-    'Brochure for companies (2023)'),
+    'Broschüre für Firmen (2024)',
+    'Brochure for companies (2024)'),
 
   'documents_guide' => array(
-    'Messeführer (2022)',
-    'Expo Guide (2022)'),
+    'Messeführer (2023)',
+    'Expo Guide (2023)'),
 
   'documents_termsandconditions' => array(
     'AGB',
