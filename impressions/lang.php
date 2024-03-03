4ac70e3c8d7d00533f6bfd362d08fc2bb51fcd67
StefanPSchmid <schmiste@ethz.ch>
2024-03-03 16:47:25 +0100

    Update impressions page to make it year-insensitive

--- impressions/lang.php
+++ impressions/lang.php
@@ -1,14 +1,14 @@
 <?php
 
 // GENERAL TRANSLATIONS FOR THIS PAGE
-$lang['general']['page_title'] = array('Chemtogether 2022 - Impressionen', 'Chemtogether 2022 - Impressions');
+$lang['general']['page_title'] = array('Chemtogether 2024 - Impressionen', 'Chemtogether 2024 - Impressions');
 $lang['navigation']['title'] = array('Impressionen', 'Impressions');
 $lang['navigation']['topnav_title']= array('Impressionen', 'Impressions');
 
 // CONTENT TRANSLATIONS FOR THIS PAGE
 $lang['content']['intro_text'] = array(
-    'Im vorletzten Jahr zählte die Chemtogether rund 500 Besucher an den zwei Messetagen im HCI Gebäude der ETH Hönggerberg. Hier findest Du Impressionen vom Messebereich und dem Begleitprogramm.',
-    'The Chemtogether 2019 counted 500 visitors during its two fair days in the HCI building at ETH Hönggerberg. Here you can find impressions of the exhibition area and the accompanying events.');
+    'Die Chemtogether zählt regelmässig rund 500 Besucher im HCI Gebäude der ETH Hönggerberg. Hier findest Du Impressionen vom Messebereich und dem Begleitprogramm.',
+    'Chemtogether regularly counts 500 visitors during its fair in the HCI building at ETH Hönggerberg. Here you can find impressions of the exhibition area and the accompanying events.');
 
 $lang['content']['gallery_subsection'] = array('Galerie','Gallery');
   
