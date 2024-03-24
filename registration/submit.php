837223166c45d3a48e90e815f82ee7cb9801d5a8
StefanPSchmid <schmiste@ethz.ch>
2024-03-24 14:10:34 +0100

    Debug registration

--- registration/submit.php
+++ registration/submit.php
@@ -18,16 +18,16 @@ if ($_POST["token"] != getenv("REGISTRATION_TOKEN")){
 
 $db = new SQLite3('registrations.db');
 
-$create_query = "CREATE TABLE IF NOT EXISTS registrations (id INTEGER PRIMARY KEY, company TEXT)";
+$create_query = "CREATE TABLE IF NOT EXISTS registrations (id INTEGER PRIMARY KEY, company TEXT, first_name TEXT)";
 $db->exec($create_query);
 
-#$company = $_POST['company'];
+$insert_query = $db->prepare("INSERT INTO registrations (company, first_name) VALUES (:company, :first_name)");
+$insert_query->bindValue(':company', $_POST['company']);
+$insert_query->bindValue(':first_name', $_POST['first_name']);
+$insert_query->execute();
 
-#$insert_query = "INSERT INTO registrations (company) VALUES ($company)";
-#$db->exec($insert_query);
+$mail_msg = "A new company has registered: $company";
 
-#$mail_msg = "A new company has registered: $company";
-
-#mail("schmiste@chem.ethz.ch", "New Chemtogether registration", $mail_msg);
+mail("schmiste@chem.ethz.ch", "New Chemtogether registration", $mail_msg);
 
 ?>
