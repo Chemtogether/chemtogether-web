4f6d0dc04f93e88b884ba9b10e80c4ba9115ff19
StefanPSchmid <schmiste@ethz.ch>
2024-03-24 14:04:22 +0100

    Debug registration

--- registration/submit.php
+++ registration/submit.php
@@ -21,13 +21,13 @@ $db = new SQLite3('registrations.db');
 $create_query = "CREATE TABLE IF NOT EXISTS registrations (id INTEGER PRIMARY KEY, company TEXT)";
 $db->exec($create_query);
 
-$company = $_POST['company'];
+#$company = $_POST['company'];
 
-$insert_query = "INSERT INTO registrations (company) VALUES ($company)";
-$db->exec($insert_query);
+#$insert_query = "INSERT INTO registrations (company) VALUES ($company)";
+#$db->exec($insert_query);
 
-$mail_msg = "A new company has registered: $company";
+#$mail_msg = "A new company has registered: $company";
 
-mail("schmiste@chem.ethz.ch", "New Chemtogether registration", $mail_msg);
+#mail("schmiste@chem.ethz.ch", "New Chemtogether registration", $mail_msg);
 
 ?>
