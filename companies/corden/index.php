27038f461dc1669fcce328a33bc6a69ad094f348
StefanPSchmid <schmiste@ethz.ch>
2024-11-07 18:51:31 +0100

    add corden employee interview

--- companies/corden/index.php
+++ companies/corden/index.php
@@ -73,11 +73,96 @@ $template['we_look'] = array(
    'title' => 'We are looking for', 
    'text' => 'Please check our career website for current opportunities.');
 
-$template['button_print_until'] = 0; 
-$template['interview_print_until'] = 0;
-if(isset($_GET['preview'])) $template['interview_print_until'] = 0;
+   $template['button_print_until'] = 0; 
 
-include('../0_template/template.php');
+   $template['interview_print_until'] = 1;
+   if(isset($_GET['preview'])) $template['interview_print_until'] = 0;
+ 
+   $i=0;
+   $template['interview'][$i++] = array(
+     'name' => 'Simon Loosli',
+     'img_url' => $awss3.'/companies/corden/interviewee_2024_1',
+     'position' => 'Chemischer Projekt Leiter',
+   
+     'education' => array(
+       0 => array(
+         'date' => '',
+         'desc' => '',
+       ),
+     ),
+   
+     'career' => array(
+       0 => array(
+         'date' => '',
+         'desc' => '',
+       ),
+     ),
+   
+     'interview' => array(
+       0 => array(
+         'question' => 'Bitte stellen Sie sich und Ihr Unternehmen kurz vor. Welche Schwerpunkte und Ziele verfolgen Sie?',
+         'answer' => 'Mein Name ist Simon Loosli und ich arbeite bei Cordenpharma in Liestal als Chemischer Projekt Leiter. Ich habe Chemie studiert und mein PhD bei Helma Wennemers an der ETH Zürich absolviert. Cordenpharma ist ein in Europa und USA tätiges CDMO.',
+       ),
+       1 => array(
+         'question' => 'Warum haben Sie sich für dieses Unternehmen entschieden?',
+         'answer' => 'Ich habe ursprünglich eine Lehre als Chemielaborant im Kilolabor in der Roche absolviert. Mein damaliger Chemiker hat mich sehr inspiriert und ich wollte später mal einen Ähnlichen Job machen können. Deshalb habe ich dann Chemie studiert und Cordenpharma hat mir dann genau die passende Stelle geboten.',
+       ),
+       2 => array(
+         'question' => 'Welche Karrieremöglichkeiten bietet Ihr Unternehmen für Studierende und Absolventen im Bereich der Chemie, Biochemie, Chemieingenieurswesen, Pharmazie und Pharmazeutische Wissenschaften?',
+         'answer' => 'Wir bieten Positionen in organisch- analytischen Positionen in der «drug product» Entwicklung und Stellen als Prozesschemiker in der Produktion. Weiter gibt es auch Postdoc Positionen.',
+       ),
+       3 => array(
+         'question' => 'Welche Studiengänge stellen Sie bevorzugt ein? Gibt es spezielle Einstiegsprogramme oder Praktika, die Sie anbieten?',
+         'answer' => 'Chemie und Chemieenginier bevorzugt mit PhD und Master.',
+       ),
+       4 => array(
+         'question' => 'Welche Qualifikationen und Fähigkeiten suchen Sie bei Bewerbern für wissenschaftliche Positionen in Ihrem Unternehmen?',
+         'answer' => 'Teamplayer,  Entrepreneurmindset, wissenschaftliche Excellence.',
+       ),
+       5 => array(
+         'question' => 'Gibt es spezifische akademische Hintergründe oder Erfahrungen, die bevorzugt werden? Im Hinblick auf Wahlfächer? Semesterarbeiten in Forschungen? Teamarbeit? Case Studies?',
+         'answer' => 'Nein, wir sind an Vielfalt interessiert.',
+       ),
+       6 => array(
+          'question' => 'Mit welchen Problemen/Fragestellungen befassen Sie sich aktuell in Ihrem Unternehmen?',
+          'answer' => 'Digitalisierung , Green Chemistry, Effizienzsteigerung in der Prozesschemie, Erweiterung synthetischer Methoden.',
+       ),
+       7 => array(
+          'question' => 'Können Sie Einblicke in aktuelle Forschungs- und Entwicklungsprojekte Ihres Unternehmens geben? Eventuell auch Kooperationen in der Forschung mit der ETH?',
+          'answer' => 'Bsp: Synthese diverser Lipide für Moderna zur Entwicklung von «state of the art» Arzneitransportmitteln. Kooperation mit der ETH anvisiert.',
+       ),
+       8 => array(
+          'question' => 'Wie engagiert sich Ihr Unternehmen in Bezug auf Nachhaltigkeit und Umweltschutz?',
+          'answer' => 'Starker Fokus auf grüne nachhaltige effiziente chemische Prozesse im Rahmen von Kundenprojekten.',
+       ),
+       9 => array(
+          'question' => 'In welchen Regionen oder Ländern ist Ihr Unternehmen tätig? Gibt es internationale Karrieremöglichkeiten?',
+          'answer' => 'Wir sind in Europa (Schweiz, Frankreich, Italien, Deutschland und Portugal) und USA (Colorado) vertreten. Austausch innerhalb von Sites etabliert.',
+       ),
+       10 => array(
+          'question' => 'Welche Möglichkeiten bieten Sie für die berufliche Weiterentwicklung und Fortbildung Ihrer Mitarbeitenden?',
+          'answer' => 'Generell können Senior Positionen erlangt werden. Auch ist der interne Wechsel in andere Abteilungen, z.B. vom Chemischen Projekt Leiter zum Projektmanager, möglich. Diverse Weiterbildungen werden von Cordenpharma gefördert. Yellow/Green Belt Kurse, Führungs- Kommunikationskurse und extensive fachliche Weiterbildung.',
+       ),
+       11 => array(
+          'question' => 'Wie würden Sie die Unternehmenskultur in Ihrem Unternehmen beschreiben, insbesondere im Hinblick auf Teamarbeit und Zusammenarbeit?',
+          'answer' => 'Wir sind als Matrixorganisation in Projektteams aufgebaut und Teamarbeit steht im Vordergrund. Gute Zusammenarbeit ist dabei der Schlüssel zum Erfolg.',
+       ),
+       12 => array(
+          'question' => 'Bietet Ihr Unternehmen Vorteile neben der Arbeit z.B. GA/Sport- und Freizeitmöglichkeiten?',
+          'answer' => 'Die Firma bezahlt einen Anteil ans Zugticket. Zudem kann jedes Jahr Reka-Check bezogen werden. Weiter werden diverse Sportveranstaltungen unterstützt (Bsp. Badminton Turnier).',
+       ),
+       13 => array(
+          'question' => 'Können Sie den typischen Bewerbungsprozess in Ihrem Unternehmen erläutern? Haben Sie Tipps für erfolgreiche Bewerbungen?',
+          'answer' => 'Eine Bewerbung läuft typischerweise in zwei Stufe ab. Ein erstes Interview zum kennenlernen und ein zweites Interview zur fachlichen Vertiefung inkl. einem Standortrundgang.',
+       ),
+       14 => array(
+          'question' => 'Wie können interessierte Studierende und Absolventen am besten mit Ihrem Unternehmen in Kontakt treten und Ihr berufliches Netzwerk erweitern?',
+          'answer' => 'Direkt das lokale HR kontaktieren. Diese freuen sich sehr über eure Kontaktaufnahme.',
+       ),
+     ),
+   );
+ 
+ include('../0_template/template.php');
 
 
 ?>
