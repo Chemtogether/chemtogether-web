3980f51a0ddd71eb5f2473a317055b5c6d5cf179
StefanPSchmid <schmiste@ethz.ch>
2024-11-29 12:15:19 -0500

    update impag website

--- companies/impag/index.php
+++ companies/impag/index.php
@@ -60,26 +60,20 @@ $template['linkedin'] = array('https://www.linkedin.com/company/impag-ag/','IMPA
 $template['we_are'] = array(
    'title' => 'We are',
 
-   'text' => 'IMPAG is a renowned, internationally active commodity trading company with headquarters in i and subsidiaries in Germany, France, Austria, Poland and Spain. We are a
-great, dynamic team and are regularly honored with the Swiss Employer Award.');
+   'text' => 'IMPAG AG is a leading distribution company specialized in innovative chemical, ingredient, and raw materials solutions, with headquarters in Zürich. With a strong focus on reliability, quality, and sustainability, IMPAG offers tailored solutions and services that meet the highest standards. We are following our company values of competence, respect, integrity and passion in all our decisions and relationships - would it be employees, suppliers or customers, to whose successes we are passionately committed to. IMPAG’s outstanding consulting expertise enables our clients to find the best solutions for their specific needs.');
 
 $template['we_offer'] = array(
    'title' => 'We offer',
-   'text' => 'You start with us in Zurich immediately or by arrangement.
-You will sell our wide range of Performance Chemicals / CASE, with a focus on paint & coatings, and expand it further.
-You are the face and voice for our customers in Switzerland, field service approx. 30%.
-You develop customized offers and lead negotiations until the contract is concluded.
-You identify and develop new business opportunities to drive growth.');
+   'text' => 'At IMPAG, we offer thrilling career opportunities in the chemical sector where you can truly make a difference.<br>
+Imagine being part of a leading distribution company, that not only focuses on technical consultancy, but also values your individualgrowth and creativity. With us, you’ll have the chance to develop your commercial and management skills in a unique environment.<br>
+Picture yourself engaging with R&D departments across the country and across various segments, understanding their challenges, and finding ideal solutions within our extensive portfolio. You’ll connect with a wide range of chemical producers, exploring different functionalities and applications of their products. Whether your passion lies in chemical developments, sales, marketing, or technical support - your role at IMPAG will be filled with diverse topics and projects, promoting your development every step of the way.
+Join our dynamic team and be part of a company that cherishes creativity, collaboration, and continuous learning. Together, we can drive innovation and shape the future of the chemical industry in a unique way.');
 
 $template['we_look'] = array(
    'title' => 'We are looking for', 
-   'text' => 'You have a basic chemical-technical education and are interested in the
-development of raw material solutions for the paint & coatings industry. 
-You have initial experience in trading industrial goods.
-You work independently and in a structured manner with an organizational flair.
-You are flexible and can adapt quickly to changing market conditions.
-You have a high level of self-motivation and networked thinking.
-You have very good language skills in German and English.');
+   'text' => 'We are in search of passionate chemists who thrive on human interaction and are eager to enhance their commercial, communication, and negotiation skills, while providing technically sophisticated solutions to our customers.<br>
+Highly motivated and self-driven individuals with a bachelor’s or master’s degree in chemistry or chemical engineering, envisioning their future beyond the lab, are encouraged to apply.
+Solution-oriented team members with high standards, striving for excellence and embodying our core values in everything they do, will find a perfect fit with us.');
 
 $template['button_print_until'] = 0; 
 $template['interview_print_until'] = 0;
